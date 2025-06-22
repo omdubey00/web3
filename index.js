538ce04c539c4675e8d6bebdf0bfd838196046c7
Riddler <roastingguruji541@gmail.com>
2025-06-22 12:32:53 +0000

    wrote a brute force hash algo to find some specific sha

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ import crypto from 'crypto'
 const input = "theriddler"
 const input2 = "100xdevs"
 
-const hash = crypto.createHash('sha256').update(input).digest('hex')
 const hash2 = crypto.createHash('sha256').update(input2).digest('hex')
 
-console.log(hash)
-console.log(hash2)
+
+function findvalue(i) {
+	console.log(i)
+	let temp = i
+	let hash = crypto.createHash('sha256').update(i.toString()).digest('hex')
+	for (let j = 0; j < 5; ++j) {
+		if (hash[j] !== '0') {
+			return false;
+		}
+	}
+	return true;
+}
+
+let i = 596139;
+while (i >= 0) {
+	const result = findvalue(i)
+	if (result === true) {
+		console.log(i)
+		i = 'a'
+	}
+	else {
+		i++;
+	}
+}
--- readme.txt
+++ readme.txt
@@ -8,4 +8,6 @@ Imp Points :---
 1. Hashing --- It is not encryption, it is converting a given input into some random string which is sort of irreversible .
    : SHA256 - one of the hashing algorithm 
    Q: what are encodings and different number like hex, octal and bit? do a bit of reading on them ;
+   : we can use a crypto node module to do hashing using createHash function. also there exist a hash.startsWith(prefix) method which tells whether the hash starts with this prefix or not.  
+   :  hash.startsWith(prefix) ---> super important method we use to brute force find the a pattern of sha . like we did check git log
 
